9852eaee31c4d401cac8de523de19ad46af9c7c5
mb @ ts <mail@manettaberends.nl>
2017-02-09 16:22:50 +0100

    added the share function

--- skins/BeyondSocial/beyondsocial.js
+++ skins/BeyondSocial/beyondsocial.js
@@ -111,4 +111,19 @@ jQuery( function ( $ ) {
 			}
 		} );
 
+	// BS js
+
+	function shareClick(){
+		$(document).on('click','#share a', function(e){
+			e.preventDefault()
+			var currentUrl=window.location.href
+			if($(this).attr('id') == 'shareTwitter'){
+				window.open('https://twitter.com/intent/tweet?text='+currentUrl); 
+			}else if($(this).attr('id') == 'shareFacebook'){
+				window.open('https://www.facebook.com/sharer/sharer.php?u='+currentUrl); 
+			}
+		})
+	}
+	shareClick()
+
 } );
